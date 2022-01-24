e9132b0aeefb462224ffb8e601dae979895907eb
Chandan Kumar <krchandan.cs@gmail.com>
2022-01-24 10:09:25 +0530

    initial commit

---
+++ Menu.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MyAssignment
+{
+    public class Menu
+    {
+        private IList<Option> Options { get; }
+
+        public Menu()
+        {
+            Options = new List<Option>();
+        }
+
+        public void Display()
+        {
+            for (var i = 0; i < Options.Count; i++)
+            {
+                Console.WriteLine("{0}. {1}", i + 1, Options[i].Name);
+            }
+
+            var choice = Input.ReadInt("Choose an option:", min: 1, max: Options.Count);
+
+            Options[choice - 1].Callback();
+        }
+
+        public Menu Add(string option, Action callback)
+        {
+            return Add(new Option(option, callback));
+        }
+
+        public Menu Add(Option option)
+        {
+            Options.Add(option);
+            return this;
+        }
+
+        public bool Contains(string option)
+        {
+            return Options.FirstOrDefault((op) => op.Name.Equals(option)) != null;
+        }
+    }
+    public class Option
+    {
+        public string Name { get; }
+
+        public Action Callback { get; }
+
+        public Option(string name, Action callback)
+        {
+            Name = name;
+            Callback = callback;
+        }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+    public static class Output
+    {
+        public static void WriteLine(ConsoleColor color, string format, params object[] args)
+        {
+            Console.ForegroundColor = color;
+            Console.WriteLine(format, args);
+            Console.ResetColor();
+        }
+
+        public static void WriteLine(ConsoleColor color, string value)
+        {
+            Console.ForegroundColor = color;
+            Console.WriteLine(value);
+            Console.ResetColor();
+        }
+
+        public static void WriteLine(string format, params object[] args)
+        {
+            Console.WriteLine(format, args);
+        }
+
+        public static void DisplayPrompt(string format, params object[] args)
+        {
+            format = format.Trim() + " ";
+            Console.Write(format, args);
+        }
+    }
+    public static class Input
+    {
+        public static int ReadInt(string prompt, int min, int max)
+        {
+            Output.DisplayPrompt(prompt);
+            return ReadInt(min, max);
+        }
+
+        public static int ReadInt(int min, int max)
+        {
+            var value = ReadInt();
+
+            while (value < min || value > max)
+            {
+                Output.DisplayPrompt("Please enter an integer between {0} and {1} (inclusive)", min, max);
+                value = ReadInt();
+            }
+
+            return value;
+        }
+
+        public static int ReadInt()
+        {
+            var input = Console.ReadLine();
+            int value;
+
+            while (!int.TryParse(input, out value))
+            {
+                Output.DisplayPrompt("Please enter an integer");
+                input = Console.ReadLine();
+            }
+
+            return value;
+        }
+
+        public static long ReadLong(string prompt, long min, long max)
+        {
+            Output.DisplayPrompt(prompt);
+            return ReadLong(min, max);
+        }
+
+        public static long ReadLong(long min, long max)
+        {
+            var value = ReadLong();
+
+            while (value < min || value > max)
+            {
+                Output.DisplayPrompt("Please enter an integer between {0} and {1} (inclusive)", min, max);
+                value = ReadLong();
+            }
+
+            return value;
+        }
+
+        public static long ReadLong()
+        {
+            string input = Console.ReadLine();
+            long value;
+
+            while (!long.TryParse(input, out value))
+            {
+                Output.DisplayPrompt("Please enter an integer");
+                input = Console.ReadLine();
+            }
+
+            return value;
+        }
+
+        public static string ReadString(string prompt)
+        {
+            Output.DisplayPrompt(prompt);
+            return Console.ReadLine();
+        }
+
+        public static TEnum ReadEnum<TEnum>(string prompt) where TEnum : struct, IConvertible, IComparable, IFormattable
+        {
+            var type = typeof(TEnum);
+
+            if (!type.IsEnum)
+            {
+                throw new ArgumentException("TEnum must be an enumerated type");
+            }
+
+            Output.WriteLine(prompt);
+            var menu = new Menu();
+
+            var choice = default(TEnum);
+            foreach (var value in Enum.GetValues(type))
+            {
+                menu.Add(Enum.GetName(type, value), () => { choice = (TEnum)value; });
+            }
+
+            menu.Display();
+
+            return choice;
+        }
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+
+namespace MyAssignment
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            bool continueAction = true;
+            while (continueAction)
+            {
+                Console.WriteLine("*------------ Menu -------------*");
+                var menu = new Menu()
+                    .Add("Word Count", () => WordCount())
+                    .Add("Social Distnace", () => SocialDistance())
+                    .Add("Exit", () => continueAction = false);
+                menu.Display();
+            }
+        }
+
+        private static void WordCount()
+        {
+            try
+            {
+                string text = string.Empty;
+                //Reading text file from URL
+                using (HttpClient wc = new HttpClient())
+                {
+                    text = wc.GetStringAsync("https://raw.githubusercontent.com/krchandancs/Sample/master/sample.txt?_sm_au_=iVVjQ2nQtSjlPjS6tQfsjK3vssBWM").Result;
+                }
+
+                //Uncomment this block if want to read from local files.
+                #region Read From Local file
+                //string filePath = @"C:\Users\Contents\sample.txt";
+                //string text = System.IO.File.ReadAllText(filePath);
+                #endregion
+
+                string[] strarr = text.TrimEnd().Split(" ");
+                Dictionary<string, int> wordCounter = new Dictionary<string, int>();
+                foreach (string s in strarr)
+                {
+                    wordCounter[s] = wordCounter.TryGetValue(s, out int value) ? value + 1 : 1;
+                }
+                var result = wordCounter.OrderByDescending(x => x.Value);
+                Console.WriteLine("*----------- Result ------------*");
+                foreach (var item in result)
+                {
+                    Console.WriteLine($"{item.Key} : {item.Value}");
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+        private static void SocialDistance()
+        {
+            Console.WriteLine("Enter binary string : ");
+            string strcontent = Console.ReadLine();
+            Console.WriteLine("Enter distance(k): ");
+            int k = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("*----------- Result ------------*");
+            int resultcount = 0, len = strcontent.Length;
+            int zerocounter = 0;
+            if (!strcontent.Contains('1'))
+                zerocounter = 1;
+            for (int j = 0; j < len; ++j)
+            {
+                if (strcontent[j] == '0')
+                    zerocounter++;
+                else
+                    zerocounter--;
+
+                if (zerocounter == k + 1)
+                {
+                    resultcount++;
+                    zerocounter = 0;
+                }
+
+                if (zerocounter < 0)
+                    zerocounter = 0;
+            }
+            Console.WriteLine(resultcount);
+        }
+    }
+}
